01be2d37ada10ec2fc399f937ab26686105cc8b3
Jensen Huang <jensenhuang2@gmail.com>
2024-05-02 18:28:33 -0400

    support writing controller state to file

--- arcapstone_python/netmq_receive.py
+++ arcapstone_python/netmq_receive.py
@@ -1,12 +1,15 @@
-import time
 import zmq
 import json
 
 context = zmq.Context()
 socket = context.socket(zmq.PULL)
 # socket.connect("tcp://localhost:12345")
-socket.connect("tcp://192.168.1.179:12345")
 
+# Quest 3 on CMU-DEVICE
+socket.connect("tcp://172.26.188.87:12345")
+# socket.connect("tcp://192.168.1.179:12345")
+
+file = open("translation_testing_coordinates.txt", "w")
 while True:
     # Receive message from remote server
     message = socket.recv()
@@ -14,7 +17,7 @@ while True:
     # Deserialize the JSON message
     try:
         data = json.loads(message.decode())
-    except json.JSONDecodeError as e:
+    except json.JSONDecodeError:
         print("Terminating the connection...")
         socket.close()
         context.term()
@@ -25,3 +28,6 @@ while True:
     right_controller = data["RightController"]
     print("Right XYZ: " + right_controller["RightLocalPosition"],
           "Right rotation: " + right_controller["RightLocalRotation"])
+    coords = f"Right XYZ\t{right_controller['RightLocalPosition']}\tRight rotation\t{right_controller['RightLocalRotation']}\n"
+    file.write(coords)
+file.close()
